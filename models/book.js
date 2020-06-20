bc3cf6a1bb73b71fddd70105ca306bbc64205bfc
Matt Link <Linkm1993@gmail.com>
2020-06-20 19:45:48 -0400

    Re-working book module to take title, author, description, image, link, and date

--- models/book.js
+++ models/book.js
@@ -4,7 +4,21 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  synopsis: String,
+  description: {
+    type: String,
+    default: ""
+  },
+  
+  image: {
+    type: String,
+    default: "",
+  },
+
+  link: {
+    type: String,
+    default: "",
+  },
+
   date: { type: Date, default: Date.now }
 });
 
