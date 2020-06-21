47a99a0bfa3ef89a0f721150a5776ce5a38060ef
Matt Link <Linkm1993@gmail.com>
2020-06-20 20:04:19 -0400

    Added searchbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Books from "./pages/Books";
 import Nav from "./components/Nav";
+import SearchBar from "./components/SearchBar"
 
 function App() {
   return (
     <div>
       <Nav />
+      <SearchBar />
       <Books />
     </div>
   );
---
+++ client/src/components/SearchBar/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function SearchBar() {
+    return <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg" />
+
+}
